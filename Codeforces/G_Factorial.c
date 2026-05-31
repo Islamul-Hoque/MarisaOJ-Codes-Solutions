45d24a55021cd4846104fd1d3127a739d953eecb
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-31 21:20:49 +0600

    feat(Codeforces/G_Factorial.c): add solution to compute factorial for multiple test cases with N up to 20

---
+++ Codeforces/G_Factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--){
+        long long int n, Factorial=1;
+        scanf("%lld", &n);
+        for (int i = 1; i <=n; i++) {
+            Factorial *= i;
+        }
+        printf("%lld\n", Factorial);
+    }
+
+    return 0;
+}
